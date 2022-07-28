2eb4b70f5542da30bf1b59ce9d80054b128d8244
mdriyadtalukder <riyadtalukder67@gmail.com>
2022-07-28 18:18:38 +0600

    first commit

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -9,20 +9,18 @@ const Navbar = () => {
                     <div className="logo">
                         <a href="#">Logo</a>
                     </div>
-                    <input type="checkbox"  id="nav-check" />
+                    <input type="checkbox" name="" id="nav-check" />
                     <label for="nav-check" className='nav-toggler'>
                         <span></span>
                     </label>
-
                     <div className="nav">
                         <ul>
-                            <li><a href="#" className='active'>Home</a></li>
-                            <li><a href="#">About</a></li>
-                            <li><a href="#">Products</a></li>
+                            <li><a href="#">Home</a></li>
+                            <li><a href="#">Product</a></li>
+                            <li><a href="#">Services</a></li>
                             <li><a href="#">Contact</a></li>
-                            <li><a href="#">Servises</a></li>
-                            <li><a href="#">Porfolio</a></li>
                         </ul>
+
                     </div>
                 </div>
             </div>
